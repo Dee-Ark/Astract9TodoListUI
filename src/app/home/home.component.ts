3e4322a972fc010e840899f2802cd3669c2d6eb4
Dee-Ark <oluwadamilareolubode.c@gmail.com>
2022-04-25 21:25:33 +0100

    Assessment

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,50 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.css']
+// })
+// export class HomeComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
+
+
+
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private jwtHelper: JwtHelperService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  isUserAuthenticated() {
+    const token: any = localStorage.getItem("jwt");
+
+    if (token && !this.jwtHelper.isTokenExpired(token)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  logOut() {
+    localStorage.removeItem("jwt");
+  }
+
+}
---
+++ src/app/models/register.model.ts
@@ -0,0 +1,7 @@
+export class Register {
+    username: string="";
+    firstname: string ="";
+    lastname: string = "";
+    email: string="";
+    password: string="";
+}
